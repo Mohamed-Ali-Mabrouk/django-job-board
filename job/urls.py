c4915025ed8d60268411c607f1119e73b72ac388
Mohamed-Ali-Mabrouk <moodamasy@gmail.com>
2023-02-01 11:18:40 +0200

    add job views & URL

---
+++ job/urls.py
@@ -0,0 +1,7 @@
+
+from django.urls import include, path
+
+urlpatterns = [
+    
+    path('jobs/', include('job.urls')),
+]
--- job/views.py
+++ job/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def job_list (request)
+
+
+
+def job_details (request, id)
